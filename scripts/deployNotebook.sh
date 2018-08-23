223e877fe56b5f18c91390d3387292d4a2a0899a
dhirajdc <dhirajc@zimetrics.com>
2018-08-23 13:09:51 +0530

    Update deployNotebook.sh

--- scripts/deployNotebook.sh
+++ scripts/deployNotebook.sh
@@ -1,6 +1,6 @@
-curl -XPOST -d @jk_prim_1_1.json http://127.0.0.1:8090/api/notebook/import > out.txt
+curl -XPOST -d @../notebooks/jk_prim_1_1.json http://127.0.0.1:8080/api/notebook/import > out.txt
 noteid=`cat out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
 echo "uov_prim_1_1.sh,$noteid" > params.txt
-curl -XPOST -d @jk_test_1_1.json http://127.0.0.1:8090/api/notebook/import > out.txt
+curl -XPOST -d @../notebooks/jk_test_1_1.json http://127.0.0.1:8080/api/notebook/import > out.txt
 noteid=`cat out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
 echo "uov_test_1_1.sh,$noteid" >> params.txt
